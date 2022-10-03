228d7f24c0371c6abe7e8c1a93c139ab59699dbc
RobzLegz <robzlegz@gmail.com>
2022-10-03 11:43:09 +0300

    aaaaaaaaaaa

---
+++ main.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Main {
+
+  public static Scanner in = new Scanner(System.in);
+
+  public static String getText() {
+    System.out.println("Enter your own text?");
+    System.out.println("y [yes]");
+    System.out.println("n [no]");
+
+    String choice = in.nextLine();
+
+    String text;
+
+    if (choice.toLowerCase().equals("y")) {
+      System.out.println("Enter text:");
+      text = in.nextLine();
+    } else {
+      text =
+        "Ir pavasaris, daudz jaunu lapu mostas, dzimst jauni pumpuri un kukaini tiem apkart rosas";
+    }
+
+    return text;
+  }
+
+  public static char getSymbol() {
+    // Scanner sus = new Scanner(System.in);
+
+    System.out.print("Enter symbol: ");
+    String symbol = in.nextLine();
+
+    if (symbol.length() != 1) {
+      System.out.print("Invalid symbol, try again: ");
+      symbol = in.nextLine();
+    }
+
+    return symbol.toLowerCase().charAt(0);
+  }
+
+  public static void findWords(char symbol, String text) {
+    String[] parts = text.split(" ");
+    int totalEq = 0;
+
+    for (int i = 0; i < parts.length; i++) {
+      String word = parts[i];
+      String lwWord = word.toLowerCase();
+
+      if (lwWord.charAt(0) == symbol) {
+        totalEq++;
+      }
+    }
+
+    System.out.println(
+      "There are " + totalEq + " words that start with symbol " + symbol
+    );
+  }
+
+  public static void main(String[] args) {
+    String text = getText();
+
+    char symbol = getSymbol();
+
+    findWords(symbol, text);
+
+    in.close();
+  }
+}
